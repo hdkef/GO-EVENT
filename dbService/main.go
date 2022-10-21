437f2cc5b3f38c0587d6dc08cdefb7ff1a819220
fedora <hdkef11@gmail.com>
2022-10-21 22:45:14 +0700

    implement event interface

--- dbService/main.go
+++ dbService/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"dbservice/usecase"
+	"fmt"
+	"log"
+	"net"
+	"os"
 
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+)
+
+var PORT string
+
+func init() {
+	_ = godotenv.Load()
+	PORT = os.Getenv("PORT")
+}
+
+func main() {
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%s", PORT))
+	if err != nil {
+		log.Fatalln("error listening", err.Error())
+	}
+	grpcServer := grpc.NewServer()
+
+	usecase.RegisterEventLayerServer(grpcServer, &usecase.EventService{})
+
+	if err := grpcServer.Serve(listen); err != nil {
+		log.Fatalln("error grpc", err.Error())
+	}
 }
--- dbService/usecase/event.go
+++ dbService/usecase/event.go
@@ -3,10 +3,11 @@ package usecase
 import "context"
 
 type EventService struct {
+	*UnimplementedEventLayerServer
 }
 
 // GetAllEvent will get all event with pagination (lastID, limit, and query)
-func (e *EventService) GetAllEvent(ctx *context.Context, pagination *Pagination) (*EventList, error) {
+func (e *EventService) GetAllEvent(ctx context.Context, pagination *Pagination) (*EventList, error) {
 	ID := uint32(1)
 	Name := "Marathon"
 	Desc := "Pocari Sweat Marathon 42 km in Bandung"
@@ -19,4 +20,36 @@ func (e *EventService) GetAllEvent(ctx *context.Context, pagination *Pagination)
 	return &EventList{
 		List: []*Event{&example, &example, &example},
 	}, nil
+}
+
+func (e *EventService) GetAllParticipant(ctx context.Context, pagination *Pagination) (*ParticipantList, error) {
+	return nil, nil
+}
+
+func (e *EventService) GetEventByID(ctx context.Context, id *IDPayload) (*Event, error) {
+	return nil, nil
+}
+
+func (e *EventService) RegisterEvent(ctx context.Context, payload *Register) (*Empty, error) {
+	return nil, nil
+}
+
+func (e *EventService) GetRegisterByUserID(ctx context.Context, id *IDPayload) (*Register, error) {
+	return nil, nil
+}
+
+func (e *EventService) CreateSubscription(ctx context.Context, payload *Subscription) (*Empty, error) {
+	return nil, nil
+}
+
+func (e *EventService) GetAllSubscription(ctx context.Context, pagination *Pagination) (*SubscriptionList, error) {
+	return nil, nil
+}
+
+func (e *EventService) GetPaymentByUserID(ctx context.Context, id *IDPayload) (*PaymentList, error) {
+	return nil, nil
+}
+
+func (e *EventService) CreatePayment(ctx context.Context, payment *Payment) (*Empty, error) {
+	return nil, nil
 }
